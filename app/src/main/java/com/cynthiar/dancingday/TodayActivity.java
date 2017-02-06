214d4d71e1d9cf3ab88c7b1169f1a73d4d01cc00
cynthia-r <ppscyn@gmail.com>
2017-02-05 19:54:51 -0800

    Add navigation icon in app bar

--- app/src/main/java/com/cynthiar/dancingday/TodayActivity.java
+++ app/src/main/java/com/cynthiar/dancingday/TodayActivity.java
@@ -1,11 +1,13 @@
 package com.cynthiar.dancingday;
 
 import android.app.FragmentManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -29,6 +31,9 @@ public class TodayActivity extends AppCompatActivity {
     private ListView mDrawerList;
 
     private Toolbar myToolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mTitle;
+    private String mDrawerTitle = "Timeframes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +41,9 @@ public class TodayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_today);
 
         // Setup toolbar
+        mTitle = "Today";
         myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        myToolbar.setTitle("Today");
+        myToolbar.setTitle(mTitle);
         setSupportActionBar(myToolbar);
 
         // Setup navigation drawer
@@ -45,16 +51,55 @@ public class TodayActivity extends AppCompatActivity {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        // Set the adapter for the list view
+        // Set the adapter for the drawer list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.drawer_list_item, timeFrames));
         // Set the list's click listener
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
+        // Setup drawer toggle
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mDrawerTitle);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         // Add the fragment to the 'fragment_container' FrameLayout
         SingleDayFragment firstFragment = new SingleDayFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_frame, firstFragment).commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -66,6 +111,12 @@ public class TodayActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -75,6 +126,7 @@ public class TodayActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -126,7 +178,7 @@ public class TodayActivity extends AppCompatActivity {
 
     @Override
     public void setTitle(CharSequence title) {
-        //mTitle = title;
+        mTitle = title.toString();
         myToolbar.setTitle(title);
     }
 
