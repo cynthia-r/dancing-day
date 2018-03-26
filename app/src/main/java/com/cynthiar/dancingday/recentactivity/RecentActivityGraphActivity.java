1b970f215999be023063b146ddeddd3725ba2e1f
Cynthia Randriamanohisoa <CynthiaR@BitTitan.com>
2018-03-25 17:02:34 -0700

    Format graph labels

--- app/src/main/java/com/cynthiar/dancingday/recentactivity/RecentActivityGraphActivity.java
+++ app/src/main/java/com/cynthiar/dancingday/recentactivity/RecentActivityGraphActivity.java
@@ -8,19 +8,31 @@ import com.cynthiar.dancingday.R;
 import com.cynthiar.dancingday.database.ClassActivityDao;
 import com.cynthiar.dancingday.model.DummyUtils;
 import com.cynthiar.dancingday.model.classActivity.ClassActivity;
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Activity displaying the graph of recent activity.
+ */
 public class RecentActivityGraphActivity extends AppCompatActivity {
+    public static DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("EEE dd");
+    public static DateTimeFormatter localTimeFormatter = DateTimeFormat.forPattern("H'h'mm");
+    public static DateTimeFormatter localTimeFormatterNoMinutes = DateTimeFormat.forPattern("H'h'");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,48 +41,88 @@ public class RecentActivityGraphActivity extends AppCompatActivity {
 
         // Get the activity over the past 7 days - todo
         int numberOfDays = 20;
-        HashMap<String, Pair<Integer, Integer>> minutesPerDayMap = this.getPastDays(numberOfDays);
+        HashMap<Date, Pair<Integer, Integer>> minutesPerDayMap = this.getPastDays(numberOfDays);
 
         // Display the graph of activity
         if (!minutesPerDayMap.isEmpty()) {
+            // Convert the map information to data points
             DataPoint[] dataPoints = new DataPoint[numberOfDays];
-            for (String day:minutesPerDayMap.keySet()
+            Date minDate = new Date(Long.MAX_VALUE);
+            Date maxDate = new Date(Long.MIN_VALUE);
+            for (Date day:minutesPerDayMap.keySet()
                     ) {
+                if (day.before(minDate))
+                    minDate = day;
+                else if (day.after(maxDate))
+                    maxDate = day;
                 Pair<Integer, Integer> pair = minutesPerDayMap.get(day);
-                dataPoints[pair.first] = new DataPoint(pair.first, pair.second);
+                dataPoints[pair.first] = new DataPoint(day, pair.second);
             }
+
+            // Create a bar graph series
             GraphView graph = (GraphView) findViewById(R.id.graph);
             BarGraphSeries<DataPoint> series = new BarGraphSeries<>(dataPoints);
             graph.addSeries(series);
 
+            // Set label formatter
+            graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+                @Override
+                public String formatLabel(double value, boolean isValueX) {
+                    if (isValueX) {
+                        // Format x values as dates
+                        DateTime dateTime = new DateTime((long) value);
+                        return dateTime.toString(RecentActivityGraphActivity.dateTimeFormatter);
+                    } else {
+                        // Format y values as amounts of time
+                        int hours = (int)value / 60;
+                        int minutes = (int)value % 60;
+                        LocalTime localTime = new LocalTime(hours, minutes);
+                        return minutes > 0 ? localTime.toString(RecentActivityGraphActivity.localTimeFormatter)
+                                : localTime.toString(RecentActivityGraphActivity.localTimeFormatterNoMinutes);
+                    }
+                }
+            });
+
             // Set viewport to last 7 days
-            graph.getViewport().setMinX(0);
-            graph.getViewport().setMaxX(numberOfDays);
+            graph.getViewport().setMinX(minDate.getTime());
+            graph.getViewport().setMaxX(maxDate.getTime());
+            graph.getViewport().setXAxisBoundsManual(true);
+
+            // Set viewport to 0h to 3h
             graph.getViewport().setMinY(0.0);
             graph.getViewport().setMaxY(180.0);
             graph.getViewport().setYAxisBoundsManual(true);
-            graph.getViewport().setXAxisBoundsManual(true);
+
+            // Show 5 dates
+            graph.getGridLabelRenderer().setNumHorizontalLabels(5);
+
+            // Show every half hour range, and set width to fully display hours and minutes
+            graph.getGridLabelRenderer().setLabelVerticalWidth(150);
+            graph.getGridLabelRenderer().setNumVerticalLabels(7);
+
+            // Disable the human rounding to avoid pre-calculating labels
+            graph.getGridLabelRenderer().setHumanRounding(false);
         }
     }
 
-    private HashMap<String, Pair<Integer, Integer>> getPastDays(int numberOfDays) {
+    private HashMap<Date, Pair<Integer, Integer>> getPastDays(int numberOfDays) {
         // Retrieve the activities over the past specified days
         ClassActivityDao classActivityDao = new ClassActivityDao();
         Iterator<ClassActivity> classActivityIterator = classActivityDao.getActivityHistory(numberOfDays);
 
         // Group the activities per day
-        HashMap<String, Pair<Integer, Integer>> minutesPerDayMap = new HashMap();
+        HashMap<Date, Pair<Integer, Integer>> minutesPerDayMap = new HashMap();
 
         // Retrieve the current day
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, -numberOfDays);
+        calendar.add(Calendar.DATE, -numberOfDays + 1); // add 1 to include current day
 
         // Go through each day
         int dayIndex = 0;
         boolean goToNextActivity = classActivityIterator.hasNext();
         while (dayIndex < numberOfDays) {
             // Get current day string
-            String currentDayString = this.getDayString(calendar);
+            Date currentDay = calendar.getTime();
             int currentActivityAmount = 0;
 
             // Check if there is any activity left
@@ -80,10 +132,9 @@ public class RecentActivityGraphActivity extends AppCompatActivity {
 
                 // Retrieve the day this activity was
                 DateTime activityDate = classActivity.getDate();
-                String dayString = this.getDayString(activityDate);
 
                 // Check if this activity was on the current day
-                if (dayString.equals(currentDayString)) {
+                if (this.compareDates(calendar, activityDate)) {
                     currentActivityAmount = classActivity.getDanceClass().danceClassTime.getDurationInMinutes();
 
                     // Go to next activity if any (might be on the same day)
@@ -92,14 +143,14 @@ public class RecentActivityGraphActivity extends AppCompatActivity {
             }
 
             // Initialize the amount for this day
-            if (!minutesPerDayMap.containsKey(currentDayString)) {
-                minutesPerDayMap.put(currentDayString, new Pair(dayIndex, 0));
+            if (!minutesPerDayMap.containsKey(currentDay)) {
+                minutesPerDayMap.put(currentDay, new Pair(dayIndex, 0));
             }
 
             // Add the amount of minutes for that day
             if (currentActivityAmount > 0) {
-                Pair<Integer, Integer> currentPair = minutesPerDayMap.get(currentDayString);
-                minutesPerDayMap.put(currentDayString, new Pair(dayIndex, currentPair.second + currentActivityAmount));
+                Pair<Integer, Integer> currentPair = minutesPerDayMap.get(currentDay);
+                minutesPerDayMap.put(currentDay, new Pair(dayIndex, currentPair.second + currentActivityAmount));
 
                 // Continue to see if there were other activities on that same day
                 continue;
@@ -114,19 +165,14 @@ public class RecentActivityGraphActivity extends AppCompatActivity {
         return minutesPerDayMap;
     }
 
-    private String getDayString(Calendar calendar) {
-        // Build day string: Day_Name Day_Of_Month
-        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-        String dayName = DummyUtils.getDayName(calendar.get(Calendar.DAY_OF_WEEK));
-        return dayName + " " + dayOfMonth;
-    }
-    private String getDayString(DateTime dateTime) {
-        // Build day string: Day_Name Day_Of_Month
-        int dayOfMonth = dateTime.getDayOfMonth();
-        int calendarDayOfWeek = (dateTime.getDayOfWeek() + 1);
-        if (calendarDayOfWeek > 7)
-            calendarDayOfWeek = calendarDayOfWeek - 7; // calendar day starts at 1=Sunday and 7=Saturday
-        String dayName = DummyUtils.getDayName(calendarDayOfWeek);
-        return dayName + " " + dayOfMonth;
+    private boolean compareDates(Calendar calendar, DateTime dateTime) {
+        int calendarDay = (calendar.get(Calendar.DAY_OF_MONTH));
+        int jodaDay = dateTime.getDayOfMonth();
+        int calendarMonth = calendar.get(Calendar.MONTH) + 1; // calendar month is zero-based
+        int jodaMonth = dateTime.getMonthOfYear();
+        int calendarYear =calendar.get(Calendar.YEAR);
+        int jodaYear = dateTime.getYear();
+
+        return calendarDay == jodaDay && calendarMonth == jodaMonth && calendarYear == jodaYear;
     }
 }
